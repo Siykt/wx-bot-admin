8d4d801748837c338a18d433f9d559e0af24fb00
siykt <cnsiykt@163.com>
2022-08-28 23:24:23 +0800

    feat: 添加error报错

--- src/graphql/index.ts
+++ src/graphql/index.ts
@@ -2,6 +2,7 @@ import { ClientError, GraphQLClient } from 'graphql-request';
 import { useGlobSetting } from '../hooks/setting';
 import { getSdk } from './generated';
 import { getToken } from '/@/utils/auth';
+import { BasicModal } from '/@/components/Modal';
 
 const globSetting = useGlobSetting();
 export const clientHeaders = new Headers();
@@ -10,8 +11,13 @@ const graphqlClient = new GraphQLClient(globSetting.apiUrl, { headers: clientHea
 
 const client = getSdk(graphqlClient, async (action) => {
   clientHeaders.set('authorization', getToken());
-  const result = await action();
-  return result;
+  try {
+    const result = await action();
+    return result;
+  } catch (error) {
+    BasicModal.error((error as ClientError).response.errors?.[0].message || '');
+    throw (error as ClientError).response.errors?.[0].extensions?.exception;
+  }
 });
 
 export default client;

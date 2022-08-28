b5ee2715f1e0601c629e8fa8621e86e96b12dbff
siykt <cnsiykt@163.com>
2022-08-28 23:25:25 +0800

    feat: 机器人基础功能

---
+++ src/router/routes/modules/bot.ts
@@ -0,0 +1,26 @@
+import type { AppRouteModule } from '/@/router/types';
+import { LAYOUT } from '/@/router/constant';
+
+const bot: AppRouteModule = {
+  path: '/bot',
+  name: 'Bot',
+  component: LAYOUT,
+  redirect: '/bot/new',
+  meta: {
+    orderNo: 11,
+    icon: 'ant-design:android-outlined',
+    title: '自动化机器人',
+  },
+  children: [
+    {
+      path: 'new',
+      name: 'BotNew',
+      component: () => import('/@/views/bot/new.vue'),
+      meta: {
+        title: '创建机器人',
+      },
+    },
+  ],
+};
+
+export default bot;

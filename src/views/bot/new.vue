d6849aaa19a04c929035b55ebdcadae61aa1d2f5
siykt <cnsiykt@163.com>
2022-08-31 21:08:36 +0800

    feat: default table

--- src/views/bot/new.vue
+++ src/views/bot/new.vue
@@ -3,10 +3,35 @@
   import BasicButton from '/@/components/Button/src/BasicButton.vue';
   import { ref } from 'vue';
   import { BotQuery } from '/@/graphql/generated';
+  import { Table } from 'ant-design-vue';
 
   const loading = ref(false);
   const bot = ref<BotQuery['bot']>();
   const botStore = useBotStoreWithOut();
+  const columns = [
+    {
+      title: '姓名',
+      dataIndex: 'name',
+      key: 'name',
+    },
+    {
+      title: '类型',
+      dataIndex: 'type',
+      key: 'type',
+    },
+    {
+      title: '性别',
+      dataIndex: 'gender',
+      key: 'gender',
+    },
+    {
+      title: '地址',
+      dataIndex: 'address',
+      key: 'address',
+    },
+  ];
+  const status = ['未初始化', '等待扫码', '用户已存在', '已暂停'];
+
   const handleCreateBot = async () => {
     loading.value = true;
     try {
@@ -23,26 +48,39 @@
 <template>
   <div class="p-4">
     <div class="title">创建机器人</div>
-    <div>机器人状态: {{ botStore.status }}</div>
+    <div>机器人状态: {{ status[botStore.status] }}</div>
     <div v-if="botStore.scanQrcode">
       <p>WX二维码, 请确认扫码登录</p>
       <img :src="botStore.scanQrcode" alt="scanQrcode" />
     </div>
-    <BasicButton :loading="loading" @click="handleCreateBot">创建机器人</BasicButton>
-    <div v-if="bot">
-      <div>机器人好友</div>
-      <div v-for="contacts in bot.botContacts" :key="contacts.id">
-        <p>好友: {{ contacts.name }}</p>
-        <p>类型: {{ contacts.type }}</p>
-        <p>性别: {{ contacts.gender }}</p>
-        <p>地址: {{ contacts.address }}</p>
+    <div style="display: flex; justify-content: space-between; margin: 10px 0">
+      <BasicButton :loading="loading" @click="handleCreateBot">创建机器人</BasicButton>
+      <div class="search">
+        <input placeholder="请输入查找类型..." />
+        <BasicButton :loading="loading" @click="handleCreateBot">查找</BasicButton>
       </div>
     </div>
+    <Table :dataSource="bot?.botContacts ?? []" :columns="columns" />
   </div>
 </template>
 
 <style lang="less" scoped>
+  div {
+    margin: 10px 0;
+  }
+
   .title {
     font-size: 30px;
+  }
+
+  .search {
+    display: flex;
+    margin-right: 50px;
+
+    input {
+      margin-right: 10px;
+      padding-left: 5px;
+      height: 30px;
+    }
   }
 </style>
